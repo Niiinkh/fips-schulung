097346fad4f317debd8d25d41317987e563f729c
Amir Aboueldahab <amir.aboueldahab@faktorzehn.de>
2023-08-30 21:53:58 +0200

    14.4: Tests für generische Validierung der Versicherungssumme

--- modell/hausratmodell/src/test/java/org/faktorips/schulung/hausratmodell/hausrat/HausratVertragTest.java
+++ modell/hausratmodell/src/test/java/org/faktorips/schulung/hausratmodell/hausrat/HausratVertragTest.java
@@ -10,8 +10,10 @@ import org.faktorips.runtime.MessageList;
 import org.faktorips.runtime.ObjectProperty;
 import org.faktorips.runtime.Severity;
 import org.faktorips.runtime.ValidationContext;
+import org.faktorips.runtime.validation.GenericRelevanceValidation;
 import org.faktorips.values.Money;
 import org.faktorips.valueset.IntegerRange;
+import org.faktorips.valueset.MoneyRange;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +28,7 @@ public class HausratVertragTest {
 		HausratProdukt produkt = new HausratProdukt(runtimeRepository, "id", "kindId", "versionId");
 		produkt.setVorschlagVersSummeProQm(Money.euro(200));
 		produkt.setAllowedValuesForWohnflaeche(IntegerRange.valueOf(0, 100));
+		produkt.setAllowedValuesForVersSumme(MoneyRange.valueOf("0 EUR", "500 EUR"));
 		vertrag = produkt.createHausratVertrag();
 		Tarifzonentabelle tarifzonentabelle = new Tarifzonentabelle(Arrays.asList(
 				new TarifzonentabelleRow("20025", "40123", "II"), new TarifzonentabelleRow("40125", "50000", "V")));
@@ -170,7 +173,7 @@ public class HausratVertragTest {
 		Message message = messages.getMessageByCode(HausratVertrag.MSG_CODE_PRUEFE_PLZ);
 		assertThat(message).isNotNull();
 	}
-	
+
 	@Test
 	void testPruefePlz_messageAttributeSindImFehlerfallKorrektGesetzt() {
 		vertrag.setPlz("A2345");
@@ -179,10 +182,57 @@ public class HausratVertragTest {
 
 		Message message = messages.getMessageByCode(HausratVertrag.MSG_CODE_PRUEFE_PLZ);
 		assertThat(message).isNotNull();
-		assertThat(message.getText())
-				.isEqualTo("Die Postleitzahl muss genau 5 Stellen haben und numerisch sein.");
+		assertThat(message.getText()).isEqualTo("Die Postleitzahl muss genau 5 Stellen haben und numerisch sein.");
 		assertThat(message.getSeverity()).isEqualTo(Severity.ERROR);
 		assertThat(message.getInvalidObjectProperties()).map(ObjectProperty::getProperty)
 				.containsExactly(HausratVertrag.PROPERTY_PLZ);
+	}
+
+	@Test
+	void testPruefeVersicherungssumme_gueltig() {
+		vertrag.setVersSumme(Money.euro(400));
+
+		MessageList messages = vertrag.validate(context);
+
+		String messageCode = GenericRelevanceValidation.Error.ValueNotInValueSet
+				.getDefaultMessageCode(HausratVertrag.class, HausratVertrag.PROPERTY_VERSSUMME);
+		Message message = messages.getMessageByCode(messageCode);
+		assertThat(message).isNull();
+	}
+
+	@Test
+	void testPruefeVersicherungssumme_zuHoch() {
+		vertrag.setVersSumme(Money.euro(600));
+
+		MessageList messages = vertrag.validate(context);
+
+		String messageCode = GenericRelevanceValidation.Error.ValueNotInValueSet
+				.getDefaultMessageCode(HausratVertrag.class, HausratVertrag.PROPERTY_VERSSUMME);
+		Message message = messages.getMessageByCode(messageCode);
+		
+		assertThat(message).isNotNull();
+		assertThat(message.getText()).isEqualTo(
+				"Das Feld \"Versicherungssumme\" enthält einen ungültigen Wert. Der Wert muss zwischen 0.00 EUR und 500.00 EUR liegen.");
+		assertThat(message.getSeverity()).isEqualTo(Severity.ERROR);
+		assertThat(message.getInvalidObjectProperties()).map(ObjectProperty::getProperty)
+				.containsExactly(HausratVertrag.PROPERTY_VERSSUMME);
+	}
+
+	@Test
+	void testPruefeVersicherungssumme_null() {
+		vertrag.setVersSumme(null);
+		
+		MessageList messages = vertrag.validate(context);
+		
+		String messageCode = GenericRelevanceValidation.Error.MandatoryValueMissing
+				.getDefaultMessageCode(HausratVertrag.class, HausratVertrag.PROPERTY_VERSSUMME);
+		Message message = messages.getMessageByCode(messageCode);
+
+		assertThat(message).isNotNull();
+		assertThat(message.getText()).isEqualTo(
+				"Das Feld \"Versicherungssumme\" muss einen Wert enthalten.");
+		assertThat(message.getSeverity()).isEqualTo(Severity.ERROR);
+		assertThat(message.getInvalidObjectProperties()).map(ObjectProperty::getProperty)
+		.containsExactly(HausratVertrag.PROPERTY_VERSSUMME);
 	}
 }
